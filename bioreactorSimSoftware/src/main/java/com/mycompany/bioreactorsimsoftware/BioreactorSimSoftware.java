c1539f3a588d871afb46eca96f3f9a8db8338dfb
amyaea <152790031+amyaea@users.noreply.github.com>
2024-10-20 13:49:33 +0300

    Add files via upload
    Edited the project's structure and its pom.xml file to ensure that we can have .exe file of the program

---
+++ bioreactorSimSoftware/src/main/java/com/mycompany/bioreactorsimsoftware/BioreactorSimSoftware.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bioreactorsimsoftware;
+
+/**
+ *
+ * @author amyae
+ */
+public class BioreactorSimSoftware {
+
+    public static void main(String[] args) {
+        bioethanolSimProgram mp = new bioethanolSimProgram();
+        mp.setVisible(true);
+    }
+}
