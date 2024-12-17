aed6817b86caeda751c3db37063356cae42ef05a
amyaea <152790031+amyaea@users.noreply.github.com>
2024-12-17 12:45:53 +0200

    Add files via upload

---
+++ BioreactorSimulationSoftware/src/main/java/com/mycompany/bioreactorsimulationsoftware/BioreactorSimulationSoftware.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bioreactorsimulationsoftware;
+
+/**
+ *
+ * @author yegor
+ */
+public class BioreactorSimulationSoftware {
+
+    public static void main(String[] args) {
+        
+        BioreactorSimulationProgram prog = new BioreactorSimulationProgram();
+        
+        prog.setVisible(true);
+        
+    }
+}
